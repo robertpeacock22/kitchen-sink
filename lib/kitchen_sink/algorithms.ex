e1943fe60199754559dd74620d98b6a699faa8e8
Robert Peacock <robertpeacock22@gmail.com>
2018-03-02 11:03:26 -0500

    Refactor binary search algorithm to allow strategy to be passed in
    This is the first step toward creating a binary search algorithm
    that can search different ranges in different directions (i.e. when
    going high on one range, go low on the other).
    
    Since the :midpoint strategy requires some manipulation of the ranges
    to be searched, stipulating these ranges had to be moved into the
    strategy function as well.

--- lib/kitchen_sink/algorithms.ex
+++ lib/kitchen_sink/algorithms.ex
@@ -12,7 +12,7 @@ defmodule KitchenSink.Algorithms do
 
   @type binary_search_position :: integer
   @type binary_search_fit_func :: (binary_search_position, any -> :ok | :high | :low)
-  @type binary_search_strategy :: (:midpoint | :interval)
+  @type binary_search_strategy :: (:midpoint | :interval | function)
   @type binary_search_result :: {:ok, binary_search_position} | :not_found
 
   @type binary_interval_search_position :: number
@@ -98,7 +98,11 @@ defmodule KitchenSink.Algorithms do
 
   def binary_search(start, finish, fit, target, strategy \\ :midpoint)
   @spec binary_search(
-    binary_search_position, binary_search_position, binary_search_fit_func, any, binary_search_strategy
+    binary_search_position,
+    binary_search_position,
+    binary_search_fit_func,
+    any,
+    binary_search_strategy
   ) :: binary_search_result
   def binary_search(
     range_start_list,
@@ -130,50 +134,68 @@ defmodule KitchenSink.Algorithms do
     {start ++ final_start, finish ++ final_finish}
   end
 
-  defp do_binary_search(position, position, fit, target, _), do: ok_or_not_found(position, fit, target)
-  defp do_binary_search(start_list, finish_list, fit, target, :midpoint) do
-    mid_list = binary_search_midpoint(start_list, finish_list)
-    # Maintain backward-compatibility
-    mid_fit_check = if Kernel.length(mid_list) === 1, do: List.first(mid_list), else: mid_list
-    case fit.(mid_fit_check, target) do
-      :ok -> {:ok, mid_fit_check}
-      :high -> do_binary_search(bounded_increment(mid_list, finish_list), finish_list, fit, target, :midpoint)
-      :low -> do_binary_search(start_list, bounded_decrement(mid_list, start_list), fit, target, :midpoint)
-   end
+  defp do_binary_search(list_start, list_end, fit, target, :midpoint) do
+    do_binary_search(list_start, list_end, fit, target, &binary_search_midpoint/2)
   end
-  defp do_binary_search(start_list, finish_list, fit, target, :interval) do
-    mid_list = binary_search_interval(start_list, finish_list)
+  defp do_binary_search(list_start, list_end, fit, target, :interval) do
+    do_binary_search(list_start, list_end, fit, target, &binary_search_interval/2)
+  end
+  defp do_binary_search(position, position, fit, target, _), do: ok_or_not_found(position, fit, target)
+  defp do_binary_search(list_start, list_end, fit, target, strategy) when is_function(strategy) do
+    {{a, b}, list_mid, {c, d}} = strategy.(list_start, list_end)
     # Maintain backward-compatibility
-    mid_fit_check = if Kernel.length(mid_list) === 1, do: List.first(mid_list), else: mid_list
+    mid_fit_check = if Kernel.length(list_mid) === 1, do: List.first(list_mid), else: list_mid
     case fit.(mid_fit_check, target) do
       :ok -> {:ok, mid_fit_check}
-      :high -> do_binary_search(mid_list, finish_list, fit, target, :interval)
-      :low -> do_binary_search(start_list, mid_list, fit, target, :interval)
+      :high -> do_binary_search(c, d, fit, target, strategy)
+      :low -> do_binary_search(a, b, fit, target, strategy)
     end
   end
 
-  defp binary_search_midpoint(start_list, finish_list) do
-    start_list
-    |> Enum.zip(finish_list)
-    |> Enum.map(fn {start, finish} -> start + div(finish - start, 2) end)
+  defp binary_search_midpoint(list_start, list_end) do
+    list_mid =
+      list_start
+      |> Enum.zip(list_end)
+      |> Enum.map(fn {start, finish} -> start + div(finish - start, 2) end)
+
+    range_after_mid =
+      list_mid
+      |> Enum.zip(list_end)
+      |> Enum.reduce({[], []}, fn {mid, finish}, {a, b} -> {[bounded_increment(mid, finish) | a], [finish | b]} end)
+
+    range_before_mid =
+      list_start
+      |> Enum.zip(list_mid)
+      |> Enum.reduce({[], []}, fn {start, mid}, {a, b} -> {[start | a], [bounded_decrement(mid, start) | b]} end)
+
+    {range_before_mid, list_mid |> Enum.reverse(), range_after_mid}
   end
 
-  defp binary_search_interval(start_list, finish_list) do
-    start_list
-    |> Enum.zip(finish_list)
-    |> Enum.map(fn {start, finish} -> start + (finish - start) / 2.0 end)
+  defp binary_search_interval(list_start, list_end) do
+    list_mid =
+      list_start
+      |> Enum.zip(list_end)
+      |> Enum.map(fn {start, finish} -> start + (finish - start) / 2.0 end)
+
+    range_after_mid =
+      list_mid
+      |> Enum.zip(list_end)
+      |> Enum.reduce({[], []}, fn {mid, finish}, {a, b} -> {[mid | a], [finish | b]} end)
+
+    range_before_mid =
+      list_start
+      |> Enum.zip(list_mid)
+      |> Enum.reduce({[], []}, fn {start, mid}, {a, b} -> {[start | a], [mid | b]} end)
+
+    {range_before_mid, list_mid, range_after_mid}
   end
 
-  defp bounded_increment(to_increment_list, bound_list) do
-    to_increment_list
-    |> Enum.zip(bound_list)
-    |> Enum.map(fn {to_increment, bound} -> min(to_increment + 1, bound) end)
+  defp bounded_increment(to_increment, bound) do
+    min(to_increment + 1, bound)
   end
 
-  defp bounded_decrement(to_decrement_list, bound_list) do
-    to_decrement_list
-    |> Enum.zip(bound_list)
-    |> Enum.map(fn {to_decrement, bound} -> max(to_decrement - 1, bound) end)
+  defp bounded_decrement(to_decrement, bound) do
+    max(to_decrement - 1, bound)
   end
 
   defp ok_or_not_found(list, fit, target) do
